cfc5b27f44f1c356b82bce830bab42a42a9c8224
Owen Li <tianwei2@andrew.cmu.edu>
2022-10-20 20:32:04 -0400

    implemented commandline interface. Still needs debugging.

--- pathsim.cpp
+++ pathsim.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <fstream>
+
 #include <assert.h>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <stack>
-#include <queue>
 #include <list>
 #include <vector>
 #include <bitset>
@@ -20,16 +15,14 @@
 #include <chrono>
 #include <thread>
 
+#include <getopt.h>
+
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
 using std::vector;
-using std::stack;
-using std::queue;
 using std::pair;
-using std::unordered_set;
-using std::unordered_map;
-using std::multimap;
 using std::function;
 using std::min;
 using std::max;
@@ -57,7 +50,7 @@ double current_slope(path::iterator pathit) { // end() - 1 not allowed
 }
 
 
-int simulate(path p, double power, double mass, double dt) { // dt > 0
+int simulate(path p, double power, double mass, double dt, bool verbose) {
         double x = 0, y = 0, v = 0, a = 0, t = 0;
 
         auto chkpoint = p.begin();
@@ -76,10 +69,11 @@ int simulate(path p, double power, double mass, double dt) { // dt > 0
             a = (v >= 1.5)? \
                 (power - watt_needed) / (v * mass) : 1; // m/s^2
 
-            if ((print_counter++) % 10 == 0)
+            if (verbose && (print_counter++) % 10 == 0) {
                 cout << "x: " << x << "  v: " << v << "  a: " << a << \
                 "  slope: " << slope/100.0 << "%  t: " \
                 << t << "  watt needed: " << watt_needed << endl;
+            }
 
 
             // update checkpoint and slope if necessary.
@@ -87,18 +81,108 @@ int simulate(path p, double power, double mass, double dt) { // dt > 0
                 chkpoint++;
                 slope = current_slope(chkpoint);
             }
+
+            // halts simulation if v becomes negative
+            if (v < 0.0) {
+                cout << "Your bicycle started going backwards! \n";
+                break;
+            }
         }
 
         return t;
 }
 
 
-int main() {
-    vector<datapoint> p;
-    p.push_back({0, 240});
-    p.push_back({200, 243});
-    p.push_back({500, 235});
-    p.push_back({600, 240});
+/**
+ * Prints help message.
+*/
+void print_help_msg() {
+    cout << "Usage: ./bikesim DATA_FILE WATT TIME_PRECISION [OPTION]" << endl;
+    cout << "Reads pairs of (location, height) pairs from the input file, as \n"
+            "well as the specified average cycling wattage, and precision of \n"
+            "simulation in terms of elapsed seconds per step, and returns \n"
+            "the estimated time in seconds required to finish such a trip. \n";
+    cout << "\nOptions:\n";
+    cout << "  -v \t enable verbose mode, prints data during simulation.\n";
+    cout << "  -r \t realtime mode, inserts a delay between each simulation "
+            "steps, so that the simulation becomes real-time. This flag will "
+            "automatically enable verbose mode. Warning: c++ built-in function "
+            "for timed thread sleeps have limited accuracy, therefore the "
+            "simulation may not be quite real-time when precision is high.\n";
+    cout << "  -h \t prints this help message. ";
+}
 
-    simulate(p, 150, 90, 0.1);
+
+int main(int argc, char *argv[]) {
+    
+    
+    // parse required arguments
+    if (argc < 4) {
+        cout << "Too few arguments! \n";
+        return 1;
+    }
+
+    std::ifstream pathfile(argv[1]);
+    vector<datapoint> p;
+    if (pathfile.is_open()) {
+        string line;
+        while (getline(pathfile, line)) {
+            int idx = line.find(',');
+            
+            double x = strtod(line.substr(0, idx).c_str(), NULL); 
+            double y = strtod(line.substr(idx+1).c_str(), NULL);
+
+            if (!p.empty() && x <= p.back().x) {
+                cout << "Error: path cannot go backwards! \n";
+                return 1;
+            }
+
+            p.push_back({x, y});
+        }
+    } else {
+        cout << "Error: unable to open file \"" << argv[1] << "\"! \n";
+        return 1; 
+    }
+
+    double power{strtod(argv[2], NULL)};
+    if (power <= 0.0) {
+        cout << "Error: average pedaling wattage must be positive! \n";
+        return 1;
+    }
+
+    double precision{strtod(argv[3], NULL)};
+    if (precision <= 0.0) {
+        cout << "Error: time precision has to be positive! \n";
+        return 1;
+    }
+
+
+    // parses options
+    char opt;
+    bool verbose = false;
+    bool realtime = false;
+    while ((opt = getopt(argc, argv, "v:r:h")) != -1) {
+        switch (opt) {
+            
+            case 'v':
+                verbose = true;
+                break;
+
+            case 'r':
+                verbose = true;
+                realtime = true;
+                break;
+
+            case 'h':
+                print_help_msg();
+                return 0;
+
+            default: 
+                printf("bikesim: invalid option -%c. \nTry "
+                       "\'bikesim -h\' for usage. \n", opt);
+                return 1;
+        }
+    }
+
+    simulate(p, power, 90, precision, verbose);
 }
